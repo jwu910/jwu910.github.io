f4dbc569b0890de862f3a89d28d20e83da0dac3e
Joshua Wu <Joshua.wu.0910@gmail.com>
2018-03-23 00:10:31 -0700

    Set up webpack configs

---
+++ webpack.common.js
@@ -0,0 +1,44 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+
+const config = {
+  entry: {
+    index: path.resolve(__dirname, './src/index.js')
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        test: /\.js$/
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract(
+          {
+            fallback: 'style-loader',
+            use: [
+              {
+                loader: 'css-loader'
+              },
+              {
+                loader: 'sass-loader'
+              }
+            ]
+          }
+        )
+      }
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './public')
+  },
+  plugins: [
+    new ExtractTextPlugin('main.css')
+  ]
+}
+
+module.exports = {
+  config
+}
