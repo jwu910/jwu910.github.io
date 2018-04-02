3307b9875436922a77f00536dcd380fac0c09aa2
Joshua Wu <Joshua.wu.0910@gmail.com>
2018-04-01 19:30:17 -0700

    Add home-wrapper class to home component

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ import React from 'react';
 class Home extends React.Component {
   render() {
     return (
-      <div>
+      <div className="home-wrapper">
         <h1>This is the home page</h1>
       </div>
     );
