e289b9ae4bdf795cc5bcb8d673dbc7a7214dd6f6
Joshua Wu <Joshua.wu.0910@gmail.com>
2018-04-02 00:26:54 -0700

    Add header in place of solo nav bar

--- src/Main.js
+++ src/Main.js
@@ -3,17 +3,17 @@ import { Route, Switch } from 'react-router-dom';
 
 import Home from './Home';
 import Portfolio from './js/components/portfolio/Portfolio';
-import NavBar from './js/components/navigation/NavBar';
+import Header from './js/components/Header';
 
 class Main extends React.Component {
   render() {
     return (
       <div className="main-container">
-        <NavBar />
+        <Header />
         <Switch>
           <Route exact path="/" component={ Home } />
 
-          <Route path="/test" component={ NavBar } />
+          <Route path="/test" component={ Home } />
           <Route path="/portfolio" component={ Portfolio } />
         </Switch>
       </div>
